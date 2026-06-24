ade8ffe8b61346bc0948654813785610905570d5
Eddie Finol <eddiefinesse@Eddies-MacBook-Pro.local>
2026-06-24 11:04:52 -0500

    Fix: Mark all admin pages as dynamic

--- app/admin/posts/page.tsx
+++ app/admin/posts/page.tsx
@@ -1,5 +1,6 @@
-'use client';
 export const dynamic = 'force-dynamic';
+
+'use client';
 import { useState } from 'react';
 import { useRouter, useSearchParams } from 'next/navigation';
 import { getCurrentUser } from '@/lib/auth';
